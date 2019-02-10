7f2f2173a09cdf8f96f1b375adbe73048493603c
Ceda EI <ceda_ei@protonmail.com>
2019-02-11 00:57:46 +0530

    Add base structure for bot. Rename questions.txt

---
+++ bot.js
@@ -0,0 +1,21 @@
+const Telegraf = require("telegraf");
+const { BOT_API_KEY } = process.env;
+const fs = require("fs").promises;
+const commands = require("./commands");
+
+const bot = new Telegraf(BOT_API_KEY);
+
+const data = [
+	"questions",
+	"kys",
+	"insults",
+	"commands_list"
+].map(file =>
+	fs.readFile(file + ".txt", "utf-8")
+		.then(list =>
+			list.split("\n")));
+
+Promise.all(data)
+	.then(data =>
+		commands(bot, data));
+
